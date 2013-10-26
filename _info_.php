d14770bb8dc9f86feac9bc2a0da756c73b278830
xtr4nge <xtr4nge@gmail.com>
2013-10-26 13:50:44 +0100

    init

---
+++ _info_.php
@@ -0,0 +1,7 @@
+<?
+$mod_name="urlsnarf";
+$mod_version="1.0";
+$mod_author="xtr4nge";
+$mod_logs="/FruityWifi/logs/urlsnarf.log"; 
+$mod_logs_history="/FruityWifi/www/modules/urlsnarf/includes/logs/"; 
+?>
