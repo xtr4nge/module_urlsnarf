95d419059aff3b6b215c51362844ce6caa69e0ff
xtr4nge <xtr4nge@gmail.com>
2013-11-08 10:41:37 +0000

    Update _info_.php

--- _info_.php
+++ _info_.php
@@ -1,6 +1,6 @@
 <?
 $mod_name="urlsnarf";
-$mod_version="1.1";
+$mod_version="1.2";
 $mod_author="xtr4nge";
 $mod_logs="/usr/share/FruityWifi/logs/urlsnarf.log"; 
 $mod_logs_history="/usr/share/FruityWifi/www/modules/urlsnarf/includes/logs/"; 
