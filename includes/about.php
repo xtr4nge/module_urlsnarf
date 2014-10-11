5bcd3c5abe04cd7a133ff0ad125d7bf3867dcedf
xtr4nge <xtr4nge@gmail.com>
2014-10-11 18:26:33 +0100

    About page has been added

---
+++ includes/about.php
@@ -0,0 +1,3 @@
+<b>urlsnarf</b> outputs all requested URLs sniffed from HTTP traffic in CLF (Common Log Format, used by almost all web servers), suitable for offline post-processing with your favorite web log analysis tool (analog, wwwstat, etc.).
+<br><br>
+<b>Author: </b>Dug Song [dugsong@monkey.org]
