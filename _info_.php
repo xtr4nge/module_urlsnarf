45506e443ed88baddd5f1b68e53d525fec29b14d
xtr4nge <xtr4nge@gmail.com>
2014-11-03 14:33:13 -0300

    installation script has been added

--- _info_.php
+++ _info_.php
@@ -1,6 +1,6 @@
 <?
 $mod_name="urlsnarf";
-$mod_version="1.4";
+$mod_version="1.5";
 $mod_author="xtr4nge";
 $mod_logs="/usr/share/FruityWifi/logs/urlsnarf.log"; 
 $mod_logs_history="/usr/share/FruityWifi/www/modules/urlsnarf/includes/logs/"; 
@@ -9,6 +9,7 @@ $mod_isup="ps auxww | grep urlsnarf | grep -v -e grep";
 $mod_alias="URLSnarf";
 # EXEC
 $bin_danger = "/usr/share/FruityWifi/bin/danger";
+$bin_sudo = "/usr/bin/sudo";
 $bin_urlsnarf = "/usr/sbin/urlsnarf";
 $bin_killall = "/usr/bin/killall";
 $bin_echo = "/bin/echo";
